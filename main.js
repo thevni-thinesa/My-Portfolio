e331f5f9fb49ed944705bbc99445cd5cf824d0d1
Thevni <153171327+Thevni@users.noreply.github.com>
2024-06-02 15:11:23 +0530

    new updates

---
+++ main.js
@@ -0,0 +1,38 @@
+/*============================================ toggle icon navbar =======================================*/ 
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+}
+
+/*============================================ scroll section active link =======================================*/ 
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href"=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /*============================================ sticky navbar =======================================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+    
+    /*============================================ remove toggle icon and navbar =======================================*/ 
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+};
