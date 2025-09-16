3fe7877c30f68621a362dd2ecad7e2279a7d433b
Thevni <153171327+Thevni@users.noreply.github.com>
2025-09-16 15:27:59 +0530

    new updates

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-/*============================================ toggle icon navbar =======================================*/ 
-
 let menuIcon = document.querySelector('#menu-icon');
 let Navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-xmark'); // change icon to X
+    menuIcon.classList.toggle('fa-xmark'); // toggle X icon
     Navbar.classList.toggle('active'); // show/hide mobile menu
 };
-
-/*============================================ scroll section active link =======================================*/ 
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -22,30 +18,23 @@ window.onscroll = () => {
         let Id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-            });
+            navLinks.forEach(link => link.classList.remove('active'));
             let activeLink = document.querySelector('header nav a[href*=' + Id + ']');
             if(activeLink) activeLink.classList.add('active');
         }
     });
 
-    /*============================================ sticky navbar =======================================*/
+    // Sticky header
     let header = document.querySelector('header');
     header.classList.toggle('sticky', top > 100);
 
-    /*============================================ remove toggle icon and navbar on scroll =======================================*/ 
+    // Close mobile menu on scroll
     menuIcon.classList.remove('fa-xmark');
     Navbar.classList.remove('active');
 };
 
-/*============================================ scroll reveal =======================================*/ 
-ScrollReveal({
-    distance: '80px',
-    duration: 2000,
-    delay: 200,
-});
-
+// Scroll Reveal
+ScrollReveal({distance: '80px', duration: 2000, delay:200});
 ScrollReveal().reveal('.about-content, heading', { origin: 'top' });
 ScrollReveal().reveal('.about-img', { origin: 'right' });
 ScrollReveal().reveal('.about-content h1, .about-content p, .about-content h3, .social-media', { origin: 'left' });
@@ -53,7 +42,7 @@ ScrollReveal().reveal('.about-content a', { origin: 'right' });
 ScrollReveal().reveal('.skills-container, .education-box', { origin: 'left' });
 ScrollReveal().reveal('.projects-box, .contact form', { origin: 'right' });
 
-/*============================================ typed js =======================================*/ 
+// Typed JS
 const typed = new Typed('.multiple-text', {
     strings: ['Front-End Developer', 'Web Developer'],
     typeSpeed: 70,
